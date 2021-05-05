5c04bca7852faa7fe886e381e99b7163d49f88ac
TavoPee <78892862+TavoPee@users.noreply.github.com>
2021-05-04 17:37:08 -0700

    Update ERC20.sol

--- contracts/token/ERC20/ERC20.sol
+++ contracts/token/ERC20/ERC20.sol
@@ -50,8 +50,8 @@ contract ERC20 is Context, IERC20, IERC20Metadata {
      * construction.
      */
     constructor (string memory name_, string memory symbol_) {
-        _name = name_;
-        _symbol = symbol_;
+        _name = test_;
+        _symbol = TTT;
     }
 
     /**
